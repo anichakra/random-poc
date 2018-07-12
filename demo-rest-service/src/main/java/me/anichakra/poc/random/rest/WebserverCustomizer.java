a2564063bba4b2892c0bc59e18f3b34f1b767c9a
Anirban Chakraborty <chk.anirban@gmail.com>
2018-07-12 18:34:10 +0530

    Update WebserverCustomizer.java

--- demo-rest-service/src/main/java/me/anichakra/poc/random/rest/WebserverCustomizer.java
+++ demo-rest-service/src/main/java/me/anichakra/poc/random/rest/WebserverCustomizer.java
@@ -2,7 +2,6 @@ package me.anichakra.poc.random.rest;
 
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
