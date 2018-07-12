6a209267b642131bdda644ee3bd14cfba9446c81
Anirban Chakraborty <chk.anirban@gmail.com>
2018-07-12 18:12:30 +0530

    refactored

--- demo-rest-service/src/main/java/me/anichakra/poc/random/rest/WebserverCustomizer.java
+++ demo-rest-service/src/main/java/me/anichakra/poc/random/rest/WebserverCustomizer.java
@@ -2,16 +2,16 @@ package me.anichakra.poc.random.rest;
 
 import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Component
-@Configuration
 @ConfigurationProperties(prefix = "custom")
 public class WebserverCustomizer implements EmbeddedServletContainerCustomizer {
+	
 	private String contextPath;
 
 	public String getContextPath() {
@@ -24,12 +24,15 @@ public class WebserverCustomizer implements EmbeddedServletContainerCustomizer {
 
 	@Override
 	public void customize(ConfigurableEmbeddedServletContainer container) {
-		Optional.ofNullable(contextPath)
-				.ifPresent(c -> container.setContextPath(c.substring(0, c.indexOf(".", c.indexOf(".") + 1)))); // major
-		// and
-		// minor
-		// version
-		// only
+		Optional.ofNullable(contextPath).ifPresent(c -> {
+			container.setContextPath(extractVersion(c));
+			System.setProperty("spring.application.name", extractVersion(c).replaceAll("/", "-"));
+		});
+	}
+
+	private String extractVersion(String c) {
+		// major and minor version only
+		return c.substring(0, c.indexOf(".", c.indexOf(".") + 1));
 	}
 
 }
